1d168d760082491a8139c37925ec678e1d3f7648
nishan <your-email@example.com>
2025-02-11 20:14:17 +0545

    .....

--- chatapp/lib/services/auth.dart
+++ chatapp/lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/services/database.dart';
+import 'package:chatapp/services/shared_pref.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -28,6 +29,14 @@ class AuthMethods {
     UserCredential result = await firebaseAuth.signInWithCredential(credential);
 
     User? userDetails = result.user;
+    String username = userDetails!.email!.replaceAll("@gmail.com", "");
+    String firstletter = username.substring(0,1).toUpperCase();
+    await SharedPreferenceHelper().saveUserDisplayName(userDetails.displayName!);
+    await SharedPreferenceHelper().saveUserEmail(userDetails.email!);
+    await SharedPreferenceHelper().saveUserName(userDetails.uid);
+    await SharedPreferenceHelper().saveUserDisplayName(username);
+    await SharedPreferenceHelper().saveUserImage(userDetails.photoURL!);
+
 
     if (result != null) {
       Map<String, dynamic> userInfoMap = {
@@ -35,6 +44,8 @@ class AuthMethods {
         "Email": userDetails!.email,
         "Image": userDetails.photoURL,
         "Id": userDetails.uid,
+        "username":username.toUpperCase(),
+        "SearchKey":
       };
 
       await DatabaseMethods()
